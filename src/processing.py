c64785a5f2486e20867f8de1832b7380bcf2cda4
Mariya <mawa-melnikova@ya.ru>
2025-06-01 21:16:03 +0300

    fixes for homework #2

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,16 @@
-def filter_by_state(data: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
+from datetime import datetime
+from typing import Any, Dict, List
+
+
+def filter_by_state(
+    data: List[Dict[str, Any]], state: str = "EXECUTED"
+) -> List[Dict[str, Any]]:
     """Фильтрует словарь по значению ключа state."""
     return [item for item in data if item.get("state") == state]
 
 
-def sort_by_date(data: List[Dict[str, Any]], reverse_order: bool = True) -> List[Dict[str, Any]]:
+def sort_by_date(
+    data: List[Dict[str, Any]], reverse: bool = True
+) -> List[Dict[str, Any]]:
     """Сортирует список словарей по дате в указанном порядке(убывание)"""
-    return sorted(data, key=lambda x: datetime.strptime(x["date"]), reverse=reverse_order)
+    return sorted(data, key=lambda x: x["date"], reverse=reverse)
