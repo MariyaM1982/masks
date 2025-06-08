7f63f900e18e3b6ac8478b7da7a307b808dde1d8
Mariya <mawa-melnikova@ya.ru>
2025-06-08 21:19:08 +0300

    fixes for tests

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -4,22 +4,15 @@ from src.masks import get_mask_account, get_mask_card_number
 
 
 @pytest.mark.parametrize(
-    "card_number, mask_number",
-    [
-        ("7000792289606361", "7000 79** **** 6361"),
-        ("7158300734726758", "7158 30** **** 6758"),
-        ("6831982476737658", "6831 98** **** 7658"),
-        ("8990922113665229", "8990 92** **** 5229"),
-        ("5999414228426353", "5999 41** **** 6353"),
-    ],
+    "card_number, expected",
+    [("1234567890123456", "123456******3456"), ("1234567890", "1234567890"), ("", "")],
 )
-def test_mask_card_number(card_number: str, mask_number: str) -> None:
-    """Функция передает строку с номером карты"""
-    assert get_mask_card_number(card_number) == mask_number
+def test_mask_card_number(card_number: str, expected: str) -> None:
+    assert get_mask_card_number(card_number) == expected
 
 
 @pytest.mark.parametrize(
-    "acc_number, mask_bank_account",
+    "acc_number, expected",
     [
         ("73654108430135874305", "**4305"),
         ("64686473678894779589", "**9589"),
@@ -27,6 +20,6 @@ def test_mask_card_number(card_number: str, mask_number: str) -> None:
         ("73654108430135874305", "**4305"),
     ],
 )
-def test_mask_account(acc_number: str, mask_bank_account: str) -> None:
+def test_mask_account(acc_number: str, expected: str) -> None:
     """Функция передает строку с номером счета"""
-    assert get_mask_account(acc_number) == mask_bank_account
+    assert get_mask_account(acc_number) == expected
